e3fbb9fe25212310c717575bfdfb6601e7e40916
jlinares1 <juanlinares2b2@gmail.com>
2024-06-15 20:33:53 -0400

    add environment file name to main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:bitcoin_tracker_flutter/screens/price_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 
